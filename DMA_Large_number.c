9cfdd1e40350c73e6af1ab8415cec697fee20c15
Raziqkhan07 <98086591+Raziqkhan07@users.noreply.github.com>
2022-02-01 16:55:32 +0530

    Add files via upload

---
+++ DMA_Large_number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int i, j;
+    float *data;
+
+    printf("Enter total number of elements(1 to 100): ");
+    scanf("%d", &j);
+
+    // Allocates the memory for 'j' elements.
+    data = (float*) calloc(j, sizeof(float));
+    if(data == NULL)
+    {
+        printf("Error!!! memory not allocated.");
+        return 0;
+    }
+
+    // Stores the number entered by the user.
+    for(i = 0; i < j; ++i)
+    {
+        printf("Enter Number %d: ", i + 1);
+        scanf("%f", data + i);
+    }
+
+    // Loop to store largest number at address data
+    for(i = 1; i < j; ++i)
+    {
+        if(*data < *(data + i))
+            *data = *(data + i);
+    }
+    printf("Largest element = %.2f", *data);
+
+    return 0;
+}
---
+++ DMA_calloc_array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+     int n, i, *arr;
+     printf("Enter size of array: ");
+     scanf("%d",&n);
+     arr= (int*) calloc(n, sizeof(int));
+     if(arr==NULL)
+     {
+         printf("Memory not allocated.");
+         exit(0);
+     }
+
+     else
+     {
+         printf("Array elements are:\n");
+         for(i=0; i<n; i++)
+         {
+             printf("%d\t",*(arr+i));
+         }
+     }
+
+     return 0;
+}
